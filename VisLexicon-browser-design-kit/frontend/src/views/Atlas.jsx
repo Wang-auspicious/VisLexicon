5e56f0f9dee73c4fe60a2b2bf497b58bab5c558e
Wang-auspicious <zhaoyunzhe68@gmail.com>
2026-09-08 22:42:21 +0800

    fix: refresh atlas when host theme changes

--- VisLexicon-browser-design-kit/frontend/src/views/Atlas.jsx
+++ VisLexicon-browser-design-kit/frontend/src/views/Atlas.jsx
@@ -16,6 +16,6 @@ export default function Atlas() {
     sendLocale()
     frameRef.current?.addEventListener('load', sendLocale)
     return () => frameRef.current?.removeEventListener('load', sendLocale)
-  }, [locale])
+  }, [locale, theme])
   return <iframe ref={frameRef} title="效果图谱" src="/effects-atlas/index.html" style={{ width: '100%', height: '100%', minHeight: 'calc(100vh - 88px)', border: 0, display: 'block' }} />
 }
